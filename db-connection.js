a2c9c221b8d0dfe2fb2917d2048dfe3ece562c75
Aman Yadav <88414055+theamnyadav@users.noreply.github.com>
2022-03-17 11:13:59 +0530

    Add files via upload

---
+++ db-connection.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+
+const db_connection = mysql
+  .createConnection({
+    host: "localhost", // HOST NAME
+    user: "root", // USER NAME
+    database: "node_restful_api", // DATABASE NAME
+    password: "", // DATABASE PASSWORD
+  })
+  
+  .on("error", (err) => {
+    console.log("Failed to connect to Database - ", err);
+  });
+
+module.exports = db_connection;
